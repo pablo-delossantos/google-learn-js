400de89106ce00b649f26b537a0fb9b74934b93b
Pablo Ariel De Los Santos <pdelossantos@nosis.com>
2024-09-30 17:51:51 -0300

    Introduccion

---
+++ 01-intro/script.js
@@ -0,0 +1,12 @@
+// Tipo de dato numerico
+let myVariable = 100;
+
+// la funcion toString() cambia el tipo de dato por un string
+myVariable = myVariable.toString();
+
+typeof myVariable;
+
+
+//Espacio en blanco
+// JavaScript no es sensible a los espacios en blanco. Esto significa que el intérprete ignora la cantidad y el tipo (tabulaciones o espacios) de los espacios en blanco utilizado.
+
