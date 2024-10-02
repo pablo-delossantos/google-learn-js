85706c4e1a6820b3fdb832de47fb0213a5d446a4
Pablo Ariel De Los Santos <pdelossantos@nosis.com>
2024-10-02 17:43:12 -0300

    Clase hasta BigInt

--- 01-intro/script.js
+++ 01-intro/script.js
@@ -10,3 +10,14 @@ typeof myVariable;
 //Espacio en blanco
 // JavaScript no es sensible a los espacios en blanco. Esto significa que el intérprete ignora la cantidad y el tipo (tabulaciones o espacios) de los espacios en blanco utilizado.
 
+// Marcadores de posicion
+
+console.log( "The result is " + ( 2 + 4 ) + "." );
+// > The result is 6.
+
+console.log( `The result is ${ 2 + 4 }.` );
+// > The result is 6.
+
+// null es un valor primitivo en JavaScript que representa la ausencia intencional de cualquier otro valor.
+// Se utiliza para indicar que una variable no tiene un valor asignado en este momento o para borrar el valor actual de una variable.
+// Es importante distinguir null de undefined, aunque ambos indican la ausencia de un valor, tienen diferentes implicaciones.
